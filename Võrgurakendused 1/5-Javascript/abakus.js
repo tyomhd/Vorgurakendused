f4f209cbd77ae505939858fcf9ec7a12cbc4654a
tyomhd <lih_artem@hotmail.com>
2016-03-21 01:13:39 +0200

    now abakus moving 1 by 1

--- Võrgurakendused 1/5-Javascript/abakus.js
+++ Võrgurakendused 1/5-Javascript/abakus.js
@@ -1,3 +1,5 @@
+/*old method
+
 window.onload =  function reverseAll(){
     var beads = document.getElementsByClassName("bead");
 
@@ -7,6 +9,32 @@ window.onload =  function reverseAll(){
             beads[i].style.cssFloat="right";
         }else {
             beads[i].style.cssFloat="left";
+        }
+    }
+}
+*/
+
+function eventListener(event) {
+
+    var nmb = parseInt(event.target.textContent);
+    var beads = document.getElementsByClassName("bead");
+    var index =0;
+    for (var i = 0; i < beads.length; i++) {if(event.target==beads[i]){index=i;}}
+
+    if (event.target.classList.contains("bead")) {
+        if (event.target.style.cssFloat == "left") {
+
+
+            for (var i = 0; i < 6-nmb; i++) {
+                beads[index].style.cssFloat = "right";
+                index++;
+        }
+        } else {
+
+            for (var i = 0; i < 6-nmb; i++) {
+                beads[index].style.cssFloat = "left";
+                index++;
+            }
         }
     }
 }
