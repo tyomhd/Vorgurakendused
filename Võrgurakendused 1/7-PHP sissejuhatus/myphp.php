6adffb04b1e8c829a60509cb099fe349c57d6958
tyomhd <lih_artem@hotmail.com>
2016-04-03 15:02:02 +0300

    its done

--- Võrgurakendused 1/7-PHP sissejuhatus/myphp.php
+++ Võrgurakendused 1/7-PHP sissejuhatus/myphp.php
@@ -24,29 +24,7 @@ function reverseText($revtext) {
 }
 reverseText($text);
 
-
-
-
-
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-<a href="http://validator.w3.org/check?uri=referer">
-	<img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
-</a>
-<a href="http://jigsaw.w3.org/css-validator/check/referer">
-	<img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" /></a>
 
 
 </body>
