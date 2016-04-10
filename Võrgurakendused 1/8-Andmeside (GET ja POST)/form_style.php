6c5bef09a5e6d7c8ee63e70dcfca2a3654226762
tyomhd <lih_artem@hotmail.com>
2016-04-11 02:44:14 +0300

    validators added, but input type='color' is still not supported in all browsers

--- Võrgurakendused 1/8-Andmeside (GET ja POST)/form_style.php
+++ Võrgurakendused 1/8-Andmeside (GET ja POST)/form_style.php
@@ -9,11 +9,11 @@
 
 <?php
 $text_area = "Enter your text here";
-$bg_color = "#fff";
-$txt_color = "#fff";
+$bg_color = "#ffffff";
+$txt_color = "#ffffff";
 $brd_width = 0;
 $brd_style = "solid";
-$brd_color = "#fff";
+$brd_color = "#ffffff";
 $brd_radius = 0;
 if (isset($_POST['txtarea']) && $_POST['txtarea']!="Enter your text here" && $_POST['txtarea']!="") {
     $text_area=htmlspecialchars($_POST['txtarea']);
@@ -38,7 +38,7 @@ if (isset($_POST['radius']) && $_POST['radius']!="") {
 }
 
 ?>
-<style type="text/css">
+<style scoped>
     .result {
         width: 400px;
         height: 100px;
@@ -87,38 +87,62 @@ if (isset($_POST['radius']) && $_POST['radius']!="") {
 
 
         <ul>
-            <li><textarea name="txtarea" cols="30" rows="3" ><?php echo $text_area; ?></textarea></li>
+            <li><label>
+                    <textarea name="txtarea" cols="30" rows="3"><?php echo $text_area; ?></textarea>
+                </label></li>
 
-            <li><input type="color" name="bg_col" value= "<?php echo $bg_color; ?>"> Background color</li>
+            <li><label>
+                    <input type="color" name="bg_col" value="<?php echo $bg_color; ?>">
+                </label> Background color</li>
 
-            <li><input type="color" name="txt_col" value= "<?php echo $txt_color; ?>"> Text color</li>
+            <li><label>
+                    <input type="color" name="txt_col" value="<?php echo $txt_color; ?>">
+                </label> Text color</li>
 
 
 
 
-            <li><input type="number" name="width" value="<?php echo $brd_width; ?>" min="0" max="20" step="1"> Border width (0-20px)</li>
+            <li><label>
+                    <input type="number" name="width" value="<?php echo $brd_width; ?>" min="0" max="20" step="1">
+                </label> Border width (0-20px)</li>
 
-            <li><select name="style">
-            <option value="<?php if (isset($_POST['width']) && isset($_POST['style'])){echo $brd_style;} else echo "select"; ?>"><?php if (isset($_POST['style'])){echo $brd_style;} else echo "select";
+            <li><label>
+                    <select name="style">
+                        <option
+                            value="<?php if (isset($_POST['width']) && isset($_POST['style'])){echo $brd_style;} else echo "
+                            select
+                        "; ?>"><?php if (isset($_POST['style'])){echo $brd_style;} else echo "select";
                 ?></option>
-            <option value="solid">solid</option>
-            <option value="dotted">dotted</option>
-            <option value="dashed">dashed</option>
-            <option value="outset">outset</option>
-            <option value="double">double</option>
-            <option value="inset">inset</option>
-            <option value="groove">groove</option>
-            <option value="ridge">ridge</option>
+                        <option value="solid">solid</option>
+                        <option value="dotted">dotted</option>
+                        <option value="dashed">dashed</option>
+                        <option value="outset">outset</option>
+                        <option value="double">double</option>
+                        <option value="inset">inset</option>
+                        <option value="groove">groove</option>
+                        <option value="ridge">ridge</option>
 
-                </select> Border style</li>
+                    </select>
+                </label> Border style</li>
 
-            <li><input type="color" name="bcolor" value="<?php echo $brd_color; ?>"> Border color</li>
+            <li><label>
+                    <input type="color" name="bcolor" value="<?php echo $brd_color; ?>">
+                </label> Border color</li>
 
-            <li><input type="number" name="radius" value="<?php echo $brd_radius; ?>" min="0" max="100" step="1"> Border radius (0-100px)</li>
+            <li><label>
+                    <input type="number" name="radius" value="<?php echo $brd_radius; ?>" min="0" max="100" step="1">
+                </label> Border radius (0-100px)</li>
         </ul>
 
         <button type="submit">Show it</button>
     </form>
+
+<p>Validators:<br>
+    <a href="http://validator.w3.org/check?uri=referer">
+        <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
+    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+        <img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" /></a>
+</p>
 
 </body>
 </html>
