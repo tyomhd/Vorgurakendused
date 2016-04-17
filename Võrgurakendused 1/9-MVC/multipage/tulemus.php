e19cba71e22d2428769e1e50e0304ce69e71f959
tyomhd <lih_artem@hotmail.com>
2016-04-17 22:27:15 +0300

    3 done

--- Võrgurakendused 1/9-MVC/multipage/tulemus.php
+++ Võrgurakendused 1/9-MVC/multipage/tulemus.php
@@ -1,6 +1,27 @@
 <?php require_once('head.html'); ?>
 <div id="wrap">
 	<h3>Valiku tulemus</h3>
-	<p>Siia tuleb valiku tulemus, mida saab kuvada ainult PHP abil :)</p>
+	<p>
+		<?php
+		$dir = 'pildid/';
+		$images = glob($dir . '*.jpg');
+		$imgList = array();
+		foreach($images as $img){
+			$imgList[]=$img;
+		}
+		if(!empty($_GET)){
+			error_reporting(0);
+			$nr = intval($_GET['pilt'])-1;
+			if(in_array($images[intval($_GET['pilt'])-1], $imgList)) {
+				echo "<p>Thank you! Your vote counts! The picture number ".$_GET['pilt']." is my favorite too. </p>";
+			} else {
+				echo "<p>Your vote is not in the list, sorry.</p>";
+			}
+		} else {
+			echo "<p>Go back and vote, this is your obligation.</p>";
+		}
+		?>
+	</p>
 </div>
 <?php require_once('foot.html'); ?>
+
