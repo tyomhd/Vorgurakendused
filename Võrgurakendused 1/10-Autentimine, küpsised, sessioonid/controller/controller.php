22ec96e39c2975ba4fc143506b476ed1bcada3ad
tyomhd <lih_artem@hotmail.com>
2016-04-25 05:09:50 +0300

    almost done

--- Võrgurakendused 1/10-Autentimine, küpsised, sessioonid/controller/controller.php
+++ Võrgurakendused 1/10-Autentimine, küpsised, sessioonid/controller/controller.php
@@ -1,5 +1,10 @@
 
 <?php
+ini_set("session.cookie_lifetime",30*60);
+session_start();
+
+$_SESSION["favcolor"] = "yellow";
+print_r($_SESSION);
 
 require_once('head.html');
 
@@ -24,12 +29,26 @@ switch($page) {
         require_once('galerii.html');
         break;
     case "vote":
-        require_once('vote.html');
+        if (!isset($_SESSION['LAST_ACTIVITY'])){
+            require_once('vote.html');
+        }
+        else if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 86400)) {
+            session_unset();
+            session_destroy();
+            require_once('vote.html');
+        } else {
+            echo "<p style=\"color:red; text-align: center; font-weight: bold\">Sorry! you have voted for number ".$_SESSION['VOTED_FOR']." already</p>";
+            $_POST['pilt'] = $_SESSION['VOTED_FOR'];
+            require_once('tulemus.html');
+        }
         break;
     case "tulemus":
         require_once('tulemus.html');
         break;}
 
 require_once('foot.html');
+
+
+
 
 ?>
