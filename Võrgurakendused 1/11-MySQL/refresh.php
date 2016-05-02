981f64eba42d0de54c1f1a9472908922c2328ffd
tyomhd <lih_artem@hotmail.com>
2016-05-02 07:36:10 +0300

    done

---
+++ Võrgurakendused 1/11-MySQL/refresh.php
@@ -0,0 +1,11 @@
+<?php
+$host="localhost";
+$user="test";
+$pass="t3st3r123";
+$db="test";
+$connection = mysqli_connect($host, $user, $pass, $db) or die("ei saa ühendust mootoriga");
+mysqli_query($connection, "SET CHARACTER SET UTF8") or die("Ei saanud baasi utf-8-sse - ".mysqli_error($connection));
+mysqli_query($connection, "UPDATE alikhach_loomaaed SET vanus=vanus+1");
+mysqli_close($connection);
+header("Refresh:0; url=zoo.php");
+?>
--- Võrgurakendused 1/11-MySQL/zoo.php
+++ Võrgurakendused 1/11-MySQL/zoo.php
@@ -38,8 +38,55 @@ if ($result->num_rows > 0) {
     echo "Table is empty";
 }
 
-mysqli_close($connection);
+echo "<p><b>Hankida kõigi mingis ühes kindlas puuris elavate loomade nimi ja puuri number</b></p>";
+$q1 = mysqli_query($connection, "SELECT * FROM alikhach_loomaaed WHERE puur='1'");
+echo "<p>Puuris number 1 elavad: </p>";
+while($row = $q1->fetch_assoc()) {
+    echo $row['nimi']." ";
+}
+$q1 = mysqli_query($connection, "SELECT * FROM alikhach_loomaaed WHERE puur='3'");
+echo "<p>Puuris number 3 elavad: </p>";
+while($row = $q1->fetch_assoc()) {
+    echo $row['nimi']." ";
+}
+$q1 = mysqli_query($connection, "SELECT * FROM alikhach_loomaaed WHERE puur='6'");
+echo "<p>Puuris number 6 elavad: </p>";
+while($row = $q1->fetch_assoc()) {
+    echo $row['nimi']." ";
+}
+
+
+
+
+echo "<p><b>Hankida vanima ja noorima looma vanused</b></p>";
+$q1=mysqlQuery("SELECT MAX(vanus) AS `max` FROM alikhach_loomaaed");
+$q2=mysqlQuery("SELECT MIN(vanus) AS `min` FROM alikhach_loomaaed");
+echo "<p>Vanim on ".$q1['max']."-aastane, ja noorim on ".$q2['min']."-aastane.</p>";
+
+echo "<p><b>Hankida puuri number koos selles elavate loomade arvuga</b></p>";
+$q1=mysqlQuery("SELECT COUNT(*) AS `count` FROM alikhach_loomaaed WHERE puur='1'");
+echo "<p>Puuris number 1 loomade arv on : ".$q1['count']."</p>";
+$q1=mysqlQuery("SELECT COUNT(*) AS `count` FROM alikhach_loomaaed WHERE puur='3'");
+echo "<p>Puuris number 3 loomade arv on : ".$q1['count']."</p>";
+$q1=mysqlQuery("SELECT COUNT(*) AS `count` FROM alikhach_loomaaed WHERE puur='6'");
+echo "<p>Puuris number 6 loomade arv on : ".$q1['count']."</p>";
+$q1=mysqlQuery("SELECT COUNT(*) AS `count` FROM alikhach_loomaaed WHERE puur='7'");
+echo "<p>Puuris number 7 loomade arv on : ".$q1['count']."</p>";
+$q1=mysqlQuery("SELECT COUNT(*) AS `count` FROM alikhach_loomaaed WHERE puur='11'");
+echo "<p>Puuris number 11 loomade arv on : ".$q1['count']."</p>";
+    mysqli_close($connection);
+
+function mysqlQuery($query) {
+    $result = mysqli_query($GLOBALS['connection'], $query);
+    return $result->fetch_assoc();
+}
 
 ?>
+<form method="post" action="refresh.php">
+<button type="submit" formaction="refresh.php">Suurendada kõiki tabelis olevaid vanuseid 1 aasta võrra</button>
+</form>
+<form method="post" action="refresh2.php">
+    <button type="submit" formaction="refresh2.php">Vähendada kõiki tabelis olevaid vanuseid 1 aasta võrra</button>
+</form>
 </body>
 </html>
